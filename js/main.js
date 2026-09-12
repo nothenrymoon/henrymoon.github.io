d09287d8065b2e3077462ce64b6a50abc745773e
newmaker <owowhatsthisuwunotices@gmail.com>
2026-09-12 13:56:24 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
-await Promise.all(
-    [...document.querySelectorAll("[data-component]")].map(async element => {
-        element.innerHTML = await (
-            await fetch(element.dataset.component)
-        ).text();
-    })
-);
+document.querySelectorAll("[data-component]").forEach(async element => {
+    const response = await fetch(element.dataset.component);
+    element.innerHTML = await response.text();
 
-const imgElement = document.getElementById("coverimage");
-if (img) {
-    const hour = new Date().getHours();
+    const imgElement = document.getElementById("coverimage");
+    if (img) {
+        const hour = new Date().getHours();
 
-    const githubLink = "https://nothenrymoon.github.io/images/header_banner/";
-    const imageChosen = "";
+        const githubLink = "https://nothenrymoon.github.io/images/header_banner/";
+        const imageChosen = "";
     
-    if (hour >= 6 && hour < 12) {
-        imageChosen = "afternoon.jpg";
-    } else if (hour >= 12 && hour < 18) {
-        imageChosen = "afternoon.jpg";
-    } else if (hour >= 18 && hour < 22) {
-        imageChosen = "afternoon.jpg";
-    } else {
-        imageChosen = "afternoon.jpg";
+        if (hour >= 6 && hour < 12) {
+            imageChosen = "afternoon.jpg";
+        } else if (hour >= 12 && hour < 18) {
+            imageChosen = "afternoon.jpg";
+        } else if (hour >= 18 && hour < 22) {
+            imageChosen = "afternoon.jpg";
+        } else {
+            imageChosen = "afternoon.jpg";
+        }
+
+        imgElement.src = githubLink.concat(imageChosen);
+
+        console.log("Hello, world!");
+        console.log(githubLink.concat(imageChosen););
     }
-
-    imgElement.src = githubLink.concat(imageChosen);
-
-    console.log("Hello, world!");
-    console.log(githubLink.concat(imageChosen););
-}
+});
